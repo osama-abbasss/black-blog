d74644a6ca4cc014056cc93ae6d79d65a88e0b59
osama abbas <osama_abbass@protonmail.com>
2019-10-15 03:08:34 +0200

    'replace-url-with-re_path'

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
 
@@ -6,12 +6,12 @@ app_name = 'posts'
 
 
 urlpatterns = [
-    url(r'^$', views.PostListView.as_view(), name='post_list'),
-    url(r'^(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
-    url(r'^create/$', views.CreatePost.as_view(), name='create_post'),
-    url(r'^(?P<pk>\d+)/edit/$', views.UpdatePost.as_view(), name='edit_post'),
-    url(r'(?P<pk>\d+)/delete/$', views.DeletePost.as_view(), name='delete_post'),
-    url(r'about/$', views.About.as_view(), name='about'),
-    url(r'(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment')
+    re_path(r'^$', views.PostListView.as_view(), name='post_list'),
+    re_path(r'^(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    re_path(r'^create/$', views.CreatePost.as_view(), name='create_post'),
+    re_path(r'^(?P<pk>\d+)/edit/$', views.UpdatePost.as_view(), name='edit_post'),
+    re_path(r'(?P<pk>\d+)/delete/$', views.DeletePost.as_view(), name='delete_post'),
+    re_path(r'about/$', views.About.as_view(), name='about'),
+    re_path(r'(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment')
 
 ]
